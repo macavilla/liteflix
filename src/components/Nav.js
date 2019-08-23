46e0b722dc080e9303e280ce9302ba24192db077
macavilla <maca.villa.a@gmail.com>
2019-08-23 18:13:39 -0300

    setup inicial

---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Logo from "../img/liteflix-logo.png"
+
+const Nav = () => {
+    return(
+        <nav>
+  
+        <a href="#"><img src={Logo} alt="Liteflix Logo" /></a>
+            <div className="nav-wrapper">
+
+            </div>
+        </nav>
+    )
+}
+
+export default Nav;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,22 @@
-import React from "react"
+import React from "react";
+import Nav from "../components/Nav";
+import Hero from "../components/Hero";
 
-export default () => <div>Hello world!</div>
+export default () => (
+
+
+
+  <div id="home" className="wrapper">
+    <h1 className="visually-hidden">Inicio de LiteFlix</h1>
+
+    <Nav />
+    <header className="hero">
+    <Hero />
+    
+    </header>
+    <main className="inner-wrapper">
+    
+    </main>
+
+  </div>
+)
