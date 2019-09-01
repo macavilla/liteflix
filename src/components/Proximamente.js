1a19aaf3504e686d72ecb8b022662f46a70e1d44
Maca <maca.villa.a@gmail.com>
2019-09-01 12:33:21 -0300

    delete unused Proximamente component and rename from ProximamenteItem to Proximamente for more clarity

--- src/components/Proximamente.js
+++ src/components/Proximamente.js
@@ -1,37 +1,96 @@
-// DELETE COMPONENT
-import React, { Component } from "react";
-import ProxItem from "./ProximamenteItem"
-
-// class Proximamente extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       items: [],
-//       isLoaded: false
-//     };
-//   }
+// import React from "react";
+import React, { Component } from "react"
+import Loader from "./Loader"
+import Icon from "./Icon"
 
 
+const API_URL = 'https://api.themoviedb.org/3/movie/upcoming?api_key=';
+const KEY = '6f26fd536dd6192ec8a57e94141f8b20';
 
-//   render() {
-//       return (
+class Proximamente extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [],
+      isLoaded: false,
+      isSaved: false,
+      isLiked: false,
+    }
+  }
 
-//         <section className="proximamente-wrapper">
-//           <h2>Proximamente</h2>
-//           <div className="flex-wrapper">
-//             <ProxItem index={0} />
-//             <ProxItem index={1} />
-//             <ProxItem index={2} />
-//             <ProxItem index={3} />
-//           </div>
+  componentDidMount() {
+    fetch(API_URL + KEY)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        })
+      })
+  }
+  render() {
+    var { isLoaded, items } = this.state
+
+    if (!isLoaded) {
+      return (
+        <div className="movie-wrapper" style={{ height: 100 }}>
+          <Loader />
+        </div>
+      )
+    } else {
+      let base_url = "https://image.tmdb.org/t/p/"
+      let item = items.results[this.props.index]
+      let title = item.original_title
+      let coverImg = base_url + "/w780/" + item.backdrop_path
+      let genreId = item.genre_ids
+      let isAdult = item.adult
+
+      // console.log("Item popular:")
+      // console.log(item)
+
+      return (
+        <div className="movie-wrapper">
+          <img
+            className="movie-cover"
+            src={coverImg}
+            index={this.props.index}
+            alt={title + "cover"}
+          />
+          <div className="on-hover">
+            <div className="button-wrapper">
 
 
-//         </section>
+              <button className="wrap-circle" aria-label="Mi Lista">
+                <Icon icon="add"></Icon>
+                <div className="circle"></div>
+              </button>
 
-//       )
 
-//   }
+              <button className="wrap-circle" aria-label="Me gusta">
+                <Icon icon="like"></Icon>
+                <div className="circle"></div>
+              </button>
+            </div>
+            <div className="button-wrapper">
+              <button className="wrap-circle" aria-label="Reproducir">
+                <Icon icon="play"></Icon>
+                <div className="circle"></div>
+              </button>
 
-// }
+            </div>
+            <div className="details">
+              <h3 className="movie-title"> {title} </h3>
+              <p className="movie-details">
+                98% Coincidencia
+                <span className="age-rating"> {isAdult ? "+16" : "ATP"} </span>
+                1 h 30 min <br /> Suspenso
+              </p>
+            </div>
+          </div>
+        </div>
+      )
+    }
+  }
+}
 
-// export default Proximamente;
+export default Proximamente
