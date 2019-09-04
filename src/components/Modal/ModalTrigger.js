1c825a9e0ca0b34992b5488e2cfa8cc53555ea8d
Maca <maca.villa.a@gmail.com>
2019-09-04 01:42:36 -0300

    humble advances the Modal component

--- src/components/Modal/ModalTrigger.js
+++ src/components/Modal/ModalTrigger.js
@@ -1,22 +1,32 @@
 //Button add -> que muestra el Modal
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 import Icon from "../Icon"
 
 
 class ModalTrigger extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
     }
     render() {
         return (
-            <button   className="modal-button">
+            // <button 
+            // style={{width: '50px', height: '50px', background: 'white'}} 
+            // onClick={this.props.showModal} 
+            // className="modal-button"
+            // >
+            //     <Icon icon="add" />
+            //     <span>
+            //     {this.props.triggerText}
+            //     </span>
+            // </button>
+
+            <button onClick={this.props.showModal} className="modal-button">
                 <Icon icon="add" />
-                <span>
-                    {this.props.triggerText}
-                </span>
-            </button>);
+                <span>{this.props.triggerText}</span>
+            </button>
+        );
     }
 }
 
