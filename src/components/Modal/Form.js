d960d7f36b95c71a8bc6019718ce06e550577a36
macavilla <maca.villa.a@gmail.com>
2019-09-10 14:36:43 -0300

    add customizable select with react-select package

--- src/components/Modal/Form.js
+++ src/components/Modal/Form.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import DragAndDrop from './DragAndDrop';
 
+import Select from 'react-select';
+
+const scaryAnimals = [
+  { label: "Animación", value: 1 },
+  { label: "Aventuras", value: 2 },
+  { label: "Ciencia Ficción", value: 3 },
+  { label: "Comedia", value: 4 },
+  { label: "Documentales", value: 5 }
+];
 
 
 export default class Form extends Component {
@@ -17,6 +26,11 @@ export default class Form extends Component {
     const value = input.value;
 
     this.setState({ [input.name]: value });
+  };
+
+  handleSelect = selectedOption => {
+    this.setState({ selectedOption });
+    console.log(`Option selected:`, selectedOption);
   };
 
   handleFormSubmit = () => {
@@ -42,33 +56,38 @@ export default class Form extends Component {
         <div id="movie-info" className="fieldset">
           <div className="field title">
             <input
-            type="text"
+              type="text"
               name="title"
               id="title"
               placeholder="NOMBRE DE LA PELICULA"
               onChange={this.handleChange}
 
             />
-{        
+
             <label htmlFor="title">
               <span className="label-text">NOMBRE DE LA PELICULA</span>
             </label>
-        
-}            </div>
+
+          </div>
 
           <div className="field">
-            <label>
-              <span className="label-text">CATEGORIA</span>
-              <select name="genre" onChange={this.handleChange}>
-                <option value="Acción">Acción</option>
-                <option value="Animación">Animación</option>
-                <option value="Aventuras">Aventuras</option>
-                <option value="Ciencia Ficción">Ciencia Ficción</option>
-                <option value="Comedia">Comedia</option>
-                <option value="Documentales">Documentales</option>
-              </select>
-            </label>
-          </div>
+          <Select options={scaryAnimals} />
+          
+          {
+              // <label>
+              //     <span className="label-text">CATEGORIA</span>
+              //     <select name="genre" onChange={this.handleChange}>
+              //       <option value="Acción">Acción</option>
+              //       <option value="Animación">Animación</option>
+              //       <option value="Aventuras">Aventuras</option>
+              //       <option value="Ciencia Ficción">Ciencia Ficción</option>
+              //       <option value="Comedia">Comedia</option>
+              //       <option value="Documentales">Documentales</option>
+              //     </select>
+              //   </label>
+
+            }          
+            </div>
         </div>
         <button type="submit">Sign In</button>
       </form>
