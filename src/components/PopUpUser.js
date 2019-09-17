fccb7704bf5dc05e7fe2693d2b0be96de44af94d
macavilla <maca.villa.a@gmail.com>
2019-09-17 14:19:26 -0300

    state isOpen: false

--- src/components/PopUpUser.js
+++ src/components/PopUpUser.js
@@ -10,7 +10,7 @@ export default class UserPopUp extends Component {
         super(props);
 
         this.state = {
-            isOpen: true
+            isOpen: false
         }
 
         this.handleClic = this.handleClic.bind(this)
